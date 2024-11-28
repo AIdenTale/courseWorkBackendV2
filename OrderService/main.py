57d2d363e2b3ec44192e9057071577ba802f48fe
Seshi <sharkyyt@yandex.ru>
2024-11-28 22:53:39 +0300

    upupupu2

--- OrderService/main.py
+++ OrderService/main.py
@@ -1,18 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from starlette.responses import JSONResponse
 from ordersService.models import CreateOrder
 from ordersService.services import OrderService
 
 app = FastAPI()
 service = OrderService()
 
-@app.post("/orders")
+
+@app.post("/create_order", status_code=201)
 def create_order(order: CreateOrder):
-    order_id = service.create_order(order)
-    return {"order_id": order_id}
+    try:
+        order_id = service.create_order(order)
+        return JSONResponse(status_code=201, content={"order_id": order_id})
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"error": "Internal Server Error", "message": str(e)})
+
 
 @app.get("/orders/{order_id}")
 def get_order(order_id: int):
     order = service.get_order(order_id)
     if not order:
-        return {"error": "Order not found"}
-    return order
+        return JSONResponse(status_code=204)  # Возвращаем 204 без текста
+    return JSONResponse(status_code=200, content=order.dict())  # Преобразуем объект в словарь
